1501fb34c6b163602b64dba32da1b133452560b9
S-Harshit <harshit19csu411@ncuindia.edu>
2021-05-31 12:53:23 +0530

    Assignment 2

---
+++ Assignmentoo/JConfig.java
@@ -0,0 +1,12 @@
+package Assignmentoo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("Assignmentoo")
+public class JConfig {
+
+}
